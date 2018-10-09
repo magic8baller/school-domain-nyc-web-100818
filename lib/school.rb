e564f75ea499c15a10b0f436eced5ba9700d97f5
magic8baller <lturis@gmail.com>
2018-10-09 18:41:57 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name, roster = {})
+    #empty roster when initialized
+    @name = name
+    @roster = roster
+  end
+
+  # {grade => [name]}
+  # roster = {9 => ["Zach Morris"]}
+
+  def add_student(name, grade)
+    #add student to school via .add_student(name, grade)
+
+    #if roster is empty, give key 'grade' a value of an array
+    @roster[grade] ||= []
+    #then push in value (name)
+    @roster[grade] << name
+  end
+
+
+  #take in an argument of a grade and return all of the students in that grade
+  def grade(grade)
+    @roster[grade]
+  end
+
+# sorted list of all the students where the strings in the student arrays are sorted alphabeticall
+#iterate until reach student array, then ascend. sort elements
+  def sort
+    @roster.each do |grade, student|
+      @roster[grade] = student.sort
+    end
+  end
+
+end
